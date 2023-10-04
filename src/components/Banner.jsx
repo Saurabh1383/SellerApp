81077918fc38b9a6fbb6cd91277393236b8611f4
suman saurabh <sumansaurabh1383@gmail.com>
2023-10-05 02:11:21 +0530

    SellerApp

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,6 +11,8 @@ import {
 import { BiPlus } from "react-icons/bi";
 
 import { bannerData } from "../data";
+import Apartment1Lg from "../assets/images/apartments/a1lg.png";
+import Apartment6Lg from "../assets/images/apartments/a6lg.png";
 
 const Banner = () => {
   return (
